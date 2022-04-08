cdd001b888db83c740c39677728115816b62188c
Aliya.Gayazova <Aliya.Gayazova@innostage-group.ru>
2022-04-09 00:27:31 +0300

    # 1 и 2 второй день алгоритмов

---
+++ leetcode/BinarySearch.cs
@@ -0,0 +1,75 @@
+﻿namespace leetcode
+{
+    public class BinarySearch
+    {
+        //Given an array of integers nums which is sorted in ascending order,
+        //and an integer target, write a function to search target in nums.
+        //If target exists, then return its index. Otherwise, return -1.
+        public int Search(int[] nums, int target)
+        {
+            var l = -1;
+            var r = nums.Length;
+            while (l < r - 1)
+            {
+                var m = (l + r) / 2;
+                if (nums[m] < target)
+                    l = m;
+                else
+                    r = m;
+            }
+            return r <= nums.Length - 1 && nums[r] == target ? r : -1;
+        }
+
+        //Input: n = 5, bad = 4
+        //Output: 4
+        //Explanation:
+        //call isBadVersion(3) -> false
+        //call isBadVersion(5) -> true
+        //call isBadVersion(4) -> true
+        //Then 4 is the first bad version.
+        public int FirstBadVersion(int n)
+        {
+            var left = 1;
+            var right = n;
+            while (left <= right)
+            {
+                var m = left + (right - left) / 2;
+                Console.WriteLine(m);
+                if (IsBadVersion(m))
+                {
+                    right = m - 1;
+                }
+                else
+                {
+                    left = m + 1;
+                }
+            }
+            return left;
+        }
+
+        //заглушка для проверки
+        private bool IsBadVersion(int version)
+        {
+            return version >= 10;
+        }
+
+        //Given a sorted array of distinct integers and a target value, return the index if the target is found.
+        ////If not, return the index where it would be if it were inserted in order.
+        public int SearchInsert(int[] nums, int target)
+        {
+            var left = -1;
+            var right = nums.Length;
+            while (left < right - 1)
+            {
+                var mid = (left + right) / 2;
+                if (nums[mid] == target)
+                    return mid;
+                if (nums[mid] < target)
+                    left = mid;
+                else
+                    right = mid;
+            }
+            return right;
+        }
+    }
+}
---
+++ leetcode/TwoPointers.cs
@@ -0,0 +1,82 @@
+﻿namespace leetcode
+{
+    public class TwoPointers
+    {
+        //Given an array, rotate the array to the right by k steps, where k is non-negative.
+        public void Rotate(int[] nums, int k)
+        {
+            k %= nums.Length;
+            var newArr = new int[nums.Length];
+            Array.Copy(nums, newArr, nums.Length);
+            if (nums.Length == 1)
+                return;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i - k >= 0)
+                {
+                    nums[i] = newArr[i - k];
+                }
+                else
+                {
+                    nums[i] = newArr[nums.Length - k + i];
+                }
+            }
+        }
+
+        //Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+        public int[] SortedSquares(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                nums[0] = (int)Math.Sqrt(nums[0]);
+                return nums;
+            }
+            int left = 0, right = 0;
+            int[] result = new int[nums.Length];
+            while (nums[right] < 0 && right < nums.Length - 1)
+                right++;
+            if (right == nums.Length - 1 && nums[right] < 0)
+            {
+                while (right >= 0)
+                {
+                    result[left++] = nums[right] * nums[right--];
+                }
+                return result;
+            }
+            else if (nums[left] >= 0)
+            {
+                while (left < nums.Length)
+                {
+                    result[left] = nums[left] * nums[left++];
+                }
+                return result;
+            }
+            else
+            {
+                int index = 0;
+                left = right - 1;
+                while (left >= 0 && right < nums.Length)
+                {
+                    if (Math.Abs(nums[left]) < Math.Abs(nums[right]))
+                    {
+                        result[index++] = nums[left] * nums[left--];
+                    }
+                    else
+                    {
+                        result[index++] = nums[right] * nums[right++];
+                    }
+                }
+                while (left >= 0)
+                {
+                    result[index++] = nums[left] * nums[left--];
+                }
+                while (right < nums.Length)
+                {
+                    result[index++] = nums[right] * nums[right++];
+                }
+
+                return result;
+            }
+        }
+    }
+}
